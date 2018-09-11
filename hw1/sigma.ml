936bc407abf73a0f7e44842c8567b79af5b0f8c4
dohyeunglee <dohyeunglee@snu.ac.kr>
2018-09-11 10:51:19 +0900

    fix sigma.ml

--- hw1/sigma.ml
+++ hw1/sigma.ml
@@ -4,7 +4,7 @@
 
 let rec _sigma a b f sum =
   if a = b then sum + f b
-  else if a > b then raise(Failure "b should be greater than a")
+  else if a > b then 0
   else _sigma (a + 1) b f (sum + f a)
 
 let sigma a b f = _sigma (a+1) b f (f a)
