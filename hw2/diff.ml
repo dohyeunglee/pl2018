e399fc58eb4cfbfe4e76e5df2fc81f29cee80554
dohyeunglee <dohyeunglee@snu.ac.kr>
2018-09-12 01:11:19 +0900

    add InvalidArgument exception for diff.ml

--- hw2/diff.ml
+++ hw2/diff.ml
@@ -10,6 +10,8 @@ type ae =
   | SUM of ae list
 
 
+exception InvalidArgument
+
 let minimize =
   function
   | SUM ->
@@ -24,7 +26,9 @@ let rec diff (ae, target) =
   | VAR _ -> CONST 0
   | POWER (x, n) when x = target -> TIMES [CONST n ; POWER (x, n - 1)]
   | POWER _ -> CONST 0
+  | SUM [] -> raise InvalidArgument
   | SUM list -> SUM (List.map (fun elem -> diff (elem, target)) list)
+  | TIMES [] -> raise InvalidArgument
   | TIMES list -> handle_times (list, target) and
 handle_times (list, target) =
   let
