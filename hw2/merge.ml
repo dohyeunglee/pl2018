7313c3af378052fb306541e5a2132b3bc2266d89
dohyeunglee <dohyeunglee@snu.ac.kr>
2018-09-12 21:57:27 +0900

    implement hw2-3, 4

---
+++ hw2/merge.ml
@@ -0,0 +1,43 @@
+type heap =
+  | EMPTY
+  | NODE of rank * value * heap * heap
+and rank = int
+and value = int
+
+exception EmptyHeap
+
+let rank = function
+  | EMPTY -> -1
+  | NODE (r, _, _, _) -> r
+
+let shake (x, lh, rh) =
+  if (rank lh) >= (rank rh) then NODE (rank rh + 1, x, lh, rh)
+  else NODE (rank lh + 1, x, rh, lh)
+
+let rec merge =
+  function
+  | (EMPTY, EMPTY) -> EMPTY
+  | ((NODE _ as n, EMPTY) | (EMPTY, (NODE _ as n))) -> n
+  | ((NODE (_, x1, _, _) as heap1), (NODE (_, x2, _, _) as heap2)) ->
+      let
+        (NODE (_, x, lh, rh), target) = if x1 > x2 then (heap2, heap1) else (heap1, heap2)
+      in
+        shake (x, lh, merge (rh, target))
+
+let insert (x, h) = merge (h, NODE(0, x, EMPTY, EMPTY))
+
+let findMin = function
+  | EMPTY -> raise EmptyHeap
+  | NODE(_, x, _, _) -> x
+
+let deleteMin = function
+  | EMPTY -> raise EmptyHeap
+  | NODE(_, x, lh, rh) -> merge (lh, rh)
+
+let a = NODE (0, 8, NODE (0, 11, EMPTY, EMPTY), EMPTY);;
+let b = NODE (0, 12, NODE (0, 15, EMPTY, EMPTY), EMPTY);;
+let c = NODE (0, 7, NODE (0, 14, EMPTY, EMPTY), EMPTY);;
+let first = NODE (1, 5, NODE (0, 6, EMPTY, EMPTY), b);;
+let second = NODE (1, 3, c, a);;
+
+
---
+++ hw2/queue.ml
@@ -0,0 +1,30 @@
+module type Queue =
+  sig
+    type element
+    type queue
+    exception EMPTY_Q
+    val emptyQ: queue
+    val enQ: queue * element -> queue
+    val deQ: queue -> element * queue
+  end
+
+module IntListQ =
+  struct
+    type element = int list
+    type queue = (int list) list * (int list) list
+    exception EMPTY_Q
+
+    let emptyQ = ([], [])
+    let enQ ((inbox, outbox), element) = (element :: inbox, outbox)
+    let deQ =
+      function
+      | ([], []) -> raise EMPTY_Q
+      | (inbox, hd :: rest) -> (hd, (inbox, rest))
+      | (inbox, []) ->
+          let
+            pop :: rest = List.rev inbox
+          in (pop, ([], rest))
+  end
+
+module ValidIntListQ = (IntListQ : Queue)
+
