92c5a995de3d6e65242c420c6c6b0ef1ae3d3462
dohyeunglee <dohyeunglee@snu.ac.kr>
2018-09-12 16:32:29 +0900

    fix tab size

--- hw1/sumprod.ml
+++ hw1/sumprod.ml
@@ -5,14 +5,14 @@
 let rec prod f j i =
   match j with
   | 1 -> f (i, 1)
-	| n when n < 1 -> 1.
+  | n when n < 1 -> 1.
   | n -> (f (i, n)) *. prod f (n - 1) i
 
 
 let rec sigma f n =
   match n with
   | 1 -> f 1
-	| n when n < 1 -> 0.
+  | n when n < 1 -> 0.
   | n -> (f n) +. sigma f (n - 1)
 
 let sumprod (f, n, k) =
